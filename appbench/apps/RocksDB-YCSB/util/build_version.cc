73675f6c5b42a934ccef51f835ed004197627902
zhangjian <zhangjian@shanghaitech.edu.cn>
2023-10-30 12:15:35 -0400

    adding params for easy automated AE run

--- appbench/apps/RocksDB-YCSB/util/build_version.cc
+++ appbench/apps/RocksDB-YCSB/util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:4a9f7bd17f2eebb03114ac63bd170531356237ca";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2023-10-27";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:3e9d8500d3db19d2de3b4409c7d3594c72f6ab66";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2023-10-30";
 const char* rocksdb_build_compile_date = __DATE__;
