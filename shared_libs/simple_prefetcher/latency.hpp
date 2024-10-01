ad936836247ae80b5fa693265a1904b468593fd7
0xDkXy <0xdkxy.pollardlee@gmail.com>
2024-10-01 13:23:43 -0400

    chore: init script to help initial the environment

--- shared_libs/simple_prefetcher/latency.hpp
+++ shared_libs/simple_prefetcher/latency.hpp
@@ -6,16 +6,16 @@
 #include <stdio.h>
 
 #ifdef GET_LATENCY
-#define GET_LATENCY_START ( \
+#define GET_LATENCY_START { \
             struct timespec ts0, ts1; \
             clock_gettime(CLOCK_MONOTONIC_RAW, &ts0); \
-        )
+        }
 
-#define GET_LATENCY_END ( \
+#define GET_LATENCY_END { \
             clock_gettime(CLOCK_MONOTONIC_RAW, &ts1); \
             time_t _latency = ts1.tv_nsec - ts0.tv_nsec; \
             printf("[LATENCY] %lld\n", (long long)_latency); \
-        )
+        }
 #else
 #define GET_LATENCY_START (void)0;
 #define GET_LATENCY_END (void)0;
